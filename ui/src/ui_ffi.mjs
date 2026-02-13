8feee305026243a23768a2d42c8e278a8e5b5478
Ryan M. Moore <rmm1047@gmail.com>
2026-02-13 16:20:19 -0500

    Show actions on plot

--- ui/src/ui_ffi.mjs
+++ ui/src/ui_ffi.mjs
@@ -2,6 +2,6 @@ import embed from "vega-embed";
 
 export function vega_embed(id, vega_lite_spec) {
   requestAnimationFrame(() => {
-    embed(id, vega_lite_spec, { actions: false });
+    embed(id, vega_lite_spec, { actions: true });
   });
 }
